13a1496f55f4aa7159b2974bf9a00b37dab7fb9d
bhartikukreja2015 <bhartikukreja2015@gmail.com>
2015-10-30 05:57:08 -0700

    second2

---
+++ src/main/webapp/Handle.js
@@ -0,0 +1,37 @@
+var req;
+var active;
+function onFoucsTextField(name)
+{
+	console.log("Processing javascript");
+	if(window.XMLHttpRequest)
+		{
+		req=new XMLHttpRequest();
+		}
+	else if(window.ActiveXObject)
+		{
+		req=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	//active=document.activeElement.name;
+	var url="SuggestServlet?name="+name;
+	
+	req.onreadystatechange=getResponse;
+	req.open("GET",url,true);
+	req.send(null);
+
+
+}
+
+
+function getResponse()
+{
+if(req.readyState==4)
+	{
+	if(req.status==200)
+		{
+		//alert("hi");
+		document.getElementById("product_suggest").innerHTML=req.responseText;
+		var link=req.responseText;
+		}
+	}
+
+}
